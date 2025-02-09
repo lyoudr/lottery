ab9051bd6ec6a83ced73d067fa8f5b6941e324f2
ann.ke <lyoudr@gmail.com>
2025-02-09 15:05:03 +0800

    add deploy celery beat, celery worker, rabbitmq

--- lottery/celery.py
+++ lottery/celery.py
@@ -2,6 +2,8 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
+RABBITMQ_HOST = "rabbitmq" if os.getenv("ENV") != "local" else "localhost"
+
 # Set default Django settings moduel
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lottery.settings')
 
@@ -10,14 +12,14 @@ app = Celery('lottery')
 # Load task modules from all registered Django apps
 app.conf.update(
     # Celery
-    CELERY_BROKER_URL = "pyamqp://guest@localhost//", # RabbitMQ
+    CELERY_BROKER_URL = f"pyamqp://guest@{RABBITMQ_HOST}//", # RabbitMQ
     CELERY_ACCEPT_CONTENT = ['json'],
     CELERY_TASK_SERIALIZER = 'json',
     CELERY_TASK_ACKS_LATE = True,
     CELERY_BEAT_SCHEDULE = {
         'get_lottery_data': {
             'task': 'get_lottery_data',
-            'schedule': crontab(minute="*/1"), #crontab(minute=0, hour=0, day_of_month=1)
+            'schedule': crontab(minute="*/10"), #crontab(minute=0, hour=0, day_of_month=1)
         }
     },
     CELERY_QUEUES = {
